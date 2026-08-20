a2dbbb07864abd4366c4b7fe98af2eae3ab76bdb
Senpai404 <88324265+Senpai404X@users.noreply.github.com>
2026-08-20 11:02:48 +0530

    ('Add sitemap and robots.txt for SEO')

---
+++ app/sitemap.js
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://codeaurcareer-tyzp-lovat.vercel.app/sitemap.xml",
+  };
+}
